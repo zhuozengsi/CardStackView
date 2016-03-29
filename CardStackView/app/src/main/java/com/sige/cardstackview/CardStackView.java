78515d51447f7b0817b6e5444142f789ccf8eb2d
Zengsi Zhuo <zhuozengsi@ZhuozengsideMacBook-Pro.local>
2016-03-29 12:51:29 +0800

    Improve 1
    1. Have solved the issue of manipulate all the cardView, so we just
    move the top cardView, and it is invalid to move others.
    2. Show the specific number cardView rather than all the view we add,
    we can set up the specific number.
    3. Improve the animation of fading the top cardView.

--- CardStackView/app/src/main/java/com/sige/cardstackview/CardStackView.java
+++ CardStackView/app/src/main/java/com/sige/cardstackview/CardStackView.java
@@ -6,16 +6,17 @@ import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.MotionEvent;
-import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewConfiguration;
-import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -23,73 +24,67 @@ import java.util.List;
  */
 public class CardStackView extends FrameLayout {
 
-    private final static int ACTIVE_VIEW_NUM = 4;
-    private final static float SCALE_FACTOR = 0.97f;
+    private final static float PERSPECTIVE_FACTOR = 0.97f;
+    private final static int FADE_DURATION = 200;
     private final static String TAG = "CardStackView";
+    private final static float FADE_SLOP_RATIO = 0.2f;
+    private final static float UNCOVERED_RATIO = 0.11f;
+    private final static float CARD_WIDTH_RATIO = 0.8f;
+    private final static float CARD_HEIGHT_RATIO = 0.5f;
+    private CardView mTopCardView;
+    private int activeCardNum;
     private int mTouchSlop;
+    private int mFadeSlop;
     private int uncoveredHeight;
     private int cardViewElevation;
+    private int mScreenWidth;
     private int cardViewHeight;
-    private List<View> mViewList;
+    private int mCardViewWidth;
+    private LinkedList<CardView> mViewList;
 
     private OnTouchListener cardViewTouchListener = new OnTouchListener() {
 
         private int initialX;
         private int currentX;
-        private ObjectAnimator animator;
-        private VelocityTracker tracker = VelocityTracker.obtain();
-
         @Override
         public boolean onTouch(View v, MotionEvent event) {
-//            Log.i(TAG, v.toString() + ": touch");
+            if(v == mTopCardView) {
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        initialX = (int) event.getRawX();
+                        currentX = initialX;
+                        break;
 
-//            if(animator == null){
-//                animator = ObjectAnimator.ofFloat(v,"translationX",0,100);
-//                animator.setDuration(500);
-//                animator.start();
-//                v.getX();
-//            }
-//
-//            tracker.addMovement(event);
-//            tracker.computeCurrentVelocity(1000);
-//            float velocityX = tracker.getXVelocity();
-//            float velocityY = tracker.getYVelocity();
-//            Log.i(TAG,"velocity x: " + velocityX + "velocity y: " + velocityY);
-            int action = event.getAction();
-            switch (action){
-                case MotionEvent.ACTION_DOWN:
-                    initialX = (int)event.getRawX();
-                    currentX = initialX;
-                    break;
+                    case MotionEvent.ACTION_MOVE:
+                        int x = (int) event.getRawX();
+                        if (Math.abs(x - currentX) > mTouchSlop) {
+                            v.setTranslationX(x - initialX);
+                            currentX = x;
+                        }
+                        break;
 
-                case MotionEvent.ACTION_MOVE:
-                    int x = (int)event.getRawX();
-                    if(Math.abs(x - currentX) > mTouchSlop){
-                        v.setTranslationX(x-initialX);
-                        Log.i(TAG, "delta X:" + (x - currentX));
-                        currentX = x;
-                    }
-                    break;
-
-                case MotionEvent.ACTION_UP:
-                    Log.i(TAG,"Touch up");
-                    if(Math.abs(event.getRawX() - initialX) > 300){
-                        Log.i(TAG,"larger than mTouchSlop");
-                    }
-                    else {
-                        v.setTranslationX(0);
-                    }
-                    break;
+                    case MotionEvent.ACTION_UP:
+                        Log.i(TAG, "Touch up");
+                        x = (int) event.getRawX();
+                        if (x - initialX > mFadeSlop) {
+                            showNext(v.getX(), (mScreenWidth + mCardViewWidth / 2));
+                        } else if ((initialX - x) > mFadeSlop) {
+                            showNext(v.getX(), -(mScreenWidth + mCardViewWidth / 2));
+                        } else {
+                            v.setTranslationX(0);
+                        }
+                        break;
+                }
+                return true;
             }
-
-
-            return true;
+            return false;
         }
     };
 
 
     public CardStackView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public CardStackView(Context context, AttributeSet attrs) {
@@ -104,23 +99,26 @@ public class CardStackView extends FrameLayout {
     private void init(Context context){
 
         cardViewElevation = Math.round(getResources().getDimension(R.dimen.card_view_elevation));
-        mViewList = new ArrayList<>();
+        mViewList = new LinkedList<>();
 
         WindowManager manager = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
         DisplayMetrics metrics = new DisplayMetrics();
         manager.getDefaultDisplay().getMetrics(metrics);
-        int widthPixels = metrics.widthPixels;
+        mScreenWidth = metrics.widthPixels;
         int heightPixels = metrics.heightPixels;
-        uncoveredHeight = Math.round(heightPixels * 0.11f);
-        cardViewHeight = Math.round(heightPixels * 0.5f);
+        uncoveredHeight = Math.round(heightPixels * UNCOVERED_RATIO);
+        cardViewHeight = Math.round(heightPixels * CARD_HEIGHT_RATIO);
+        mCardViewWidth = Math.round(mScreenWidth * CARD_WIDTH_RATIO);
+        mFadeSlop = Math.round(mCardViewWidth * FADE_SLOP_RATIO);
         Log.i(TAG,"CardViewHeight: " + cardViewHeight);
         ViewConfiguration configuration = ViewConfiguration.get(context);
         mTouchSlop = configuration.getScaledTouchSlop();
-        //mTouchSlop = 1000;
+        activeCardNum = 4;
 
-        Log.i(TAG, "slop: " + mTouchSlop);
+    }
 
-
+    public void setActiveCardNum(int num){
+        activeCardNum = activeCardNum >= num ? num : activeCardNum;
     }
 
     public void setUncoveredHeight(int height){
@@ -129,34 +127,78 @@ public class CardStackView extends FrameLayout {
 
     public void addCardView(final CardView cardView) {
         Log.i(TAG, cardView.toString());
+        mTopCardView = cardView;
         cardView.setOnTouchListener(cardViewTouchListener);
-        addView(cardView);
         final int previousViewCount = mViewList.size();
-        cardView.setCardElevation(cardViewElevation * previousViewCount);
+        TextView textView = (TextView)cardView.findViewById(R.id.cardView_title);
+        textView.setText("CardView: " + previousViewCount);
+        addView(cardView);
+        Log.i(TAG,"view index: " + indexOfChild(cardView));
         LayoutParams params = (LayoutParams) cardView.getLayoutParams();
         params.height = cardViewHeight;
+        params.width = mCardViewWidth;
+        params.gravity = Gravity.CENTER_HORIZONTAL;
         cardView.setLayoutParams(params);
-        mViewList.add(cardView);
-        Log.i(TAG, "pre Card View: " + previousViewCount);
-        int firstViewMarginTop = (int) Math.round(uncoveredHeight * (1.0 - Math.pow(SCALE_FACTOR, previousViewCount)) / (1 - SCALE_FACTOR));
-        int topMargin = firstViewMarginTop;
-        for (int i = previousViewCount; i >= 0; i--) {
 
-            float scale = (float) Math.pow(SCALE_FACTOR, previousViewCount - i);
-            final View v = mViewList.get(i);
-            Log.i(TAG, "view height: " + v.getHeight());
+        if(previousViewCount < activeCardNum) {
+            //cardView.setCardElevation(cardViewElevation * previousViewCount);
+            mViewList.add(cardView);
+            Log.i(TAG, "pre Card View: " + previousViewCount);
+            layoutAgain(0,previousViewCount);
+
+        }else {
+            //cardView.setCardElevation(cardViewElevation * activeCardNum);
+            removeViewAt(0);
+            mViewList.add(cardView);
+            layoutAgain(previousViewCount-activeCardNum+1, previousViewCount);
+        }
+
+    }
+
+    private void layoutAgain(int indexMin, int indexMax){
+
+        int firstViewMarginTop = (int) Math.round(uncoveredHeight *
+                (1.0 - Math.pow(PERSPECTIVE_FACTOR, (indexMax-indexMin))) / (1 - PERSPECTIVE_FACTOR));
+
+        int topMargin = firstViewMarginTop;
+        for (int i = indexMax; i >= indexMin; i--) {
+            float scale = (float) Math.pow(PERSPECTIVE_FACTOR, indexMax - i);
+            final CardView v = mViewList.get(i);
+            v.setCardElevation((i-indexMin)*cardViewElevation);
             LayoutParams lp = (LayoutParams) v.getLayoutParams();
-            if (i < previousViewCount) {
-                topMargin = topMargin - (int) (uncoveredHeight * scale / SCALE_FACTOR);
+            if (i < indexMax) {
+                topMargin = topMargin - (int) (uncoveredHeight * scale / PERSPECTIVE_FACTOR);
             }
             lp.topMargin = topMargin;
             v.setLayoutParams(lp);
             v.setScaleX(scale);
             v.setScaleY(scale);
 
-
         }
 
+
+    }
+
+    public void addCard(CardView cardView){
+
+    }
+
+    private void showNext(float from, float to){
+
+        ObjectAnimator animator = ObjectAnimator.ofFloat(mTopCardView,"translationX",from,to);
+        animator.setDuration(FADE_DURATION);
+        animator.setInterpolator(new AccelerateInterpolator());
+        animator.start();
+        CardView temp = mViewList.getLast();
+        mViewList.removeLast();
+        mViewList.addFirst(temp);
+        mTopCardView = mViewList.getLast();
+
+
+    }
+
+    public void showNext(){
+        showNext(0, (mCardViewWidth+mScreenWidth)/2);
     }
 
     /**
@@ -172,4 +214,6 @@ public class CardStackView extends FrameLayout {
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
     }
+
+
 }
